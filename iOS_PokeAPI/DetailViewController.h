a9e44d9c3c694f2a64ba0c28b3e72927a58693b2
Simon Primetzhofer <simon.primetzhofer@live.at>
2019-02-11 16:21:45 +0100

    Second ViewController and TableView started

---
+++ iOS_PokeAPI/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  iOS_PokeAPI
+//
+//  Created by Primetzhofer on 11.02.19.
+//  Copyright © 2019 Primetzhofer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+@property(nonatomic) NSInteger row;
+@end
--- iOS_PokeAPI/ViewController.m
+++ iOS_PokeAPI/ViewController.m
@@ -7,22 +7,50 @@
 //
 
 #import "ViewController.h"
+#import "DetailViewController.h"
+#import "PokemonDatasource.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) PokemonDatasource *dataSource;
+@property (nonatomic) NSInteger selectedRow;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.dataSource = [PokemonDatasource new];
+    self.tableView.dataSource = self.dataSource;
+    self.tableView.delegate = self;
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    DetailViewController *dvc = (DetailViewController *) segue.destinationViewController;
+    dvc.row = self.selectedRow;
+}
+
+-(void) tableView: (UITableView *)tableView didSelectRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    NSLog(@"cell clicked %@", indexPath);
+    self.selectedRow = indexPath.row;
+    [self performSegueWithIdentifier:@"toDetailView" sender: self];
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setInteger: indexPath.row forKey:@"selectedRow"];
+    [defaults setObject:@"xyz" forKey:@"myKey"];
+    
+    //Defaults werden über Laufzeit hinweg gespeichert
+    //UserDefaults ist vergleichbar mit LocalStorage in JavaScript
+    
 }
 
 
